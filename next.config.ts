bdff7999d0265bd6eee4084b0cd5dff1668ac2c8
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-09 23:54:30 +0300

    config: add next rewrites to avoid cors problems

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // Proxy UI routes (public stuff)
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "production" ? "https://products-repo.onrender.com/:path*": "http://localhost:3000/:path*"
+      },
+    ]
+  },
 };
 
 export default nextConfig;
